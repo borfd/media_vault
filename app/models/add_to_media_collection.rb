e5eb27479d70d96c11ae61908c4d69f8c7e1bbc8
Boris Filipov <bfilipov@gmail.com>
2015-10-12 14:20:07 +0200

    AddToMediaCollection receives all its dependencies in constructor

--- app/models/add_to_media_collection.rb
+++ app/models/add_to_media_collection.rb
@@ -1,15 +1,15 @@
 class AddToMediaCollection
 
-  attr_reader :user
+  attr_reader :user, :url, :type
 
   def initialize(options)
     @user = options.fetch(:user)
+    @url = options.fetch(:url)
+    @type     = options.fetch(:type)
+
   end
 
-  def execute!(options)
-    url = options.fetch(:url)
-    type     = options.fetch(:type)
-
+  def execute!
     MediaRepository.add(user: user, url: url, type: type)
   end
 
--- spec/add_to_media_collection_spec.rb
+++ spec/add_to_media_collection_spec.rb
@@ -5,10 +5,10 @@ describe AddToMediaCollection do
   let(:user) { FactoryGirl.create(:user) }
   let(:item_url) { 'https://news.ycombinator.com/' }
 
-  subject { described_class.new(user: user) }
+  subject { described_class.new(user: user, url: item_url, type: 'website') }
 
   it "calls Repository with item_url" do
     expect(MediaRepository).to receive(:add).with({user: user, url: item_url, type: 'website'})
-    subject.execute!(url: item_url, type: 'website')
+    subject.execute!
   end
 end
